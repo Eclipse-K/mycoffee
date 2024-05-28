db4a7470d73419b6bf1c1ba2610004cdf0703965
Eclipse_K <godo8522@naver.com>
2024-05-28 22:57:26 +0900

    Edit Banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,9 +1,19 @@
 import "./Banner.css";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
 function Banner() {
   return (
     <div className="Banner">
       <h1>배너영역</h1>
+      <div className="Banner-container">
+        <button>
+          <SlArrowLeft />
+        </button>
+        <div></div>
+        <button>
+          <SlArrowRight />
+        </button>
+      </div>
     </div>
   );
 }
