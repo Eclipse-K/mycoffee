c40556bc6d1ae5f3b88fcf212ca0f0e32b1588e2
Eclipse_K <godo8522@naver.com>
2024-06-05 22:17:39 +0900

    Edit bestItem

--- src/Components/BestItem.js
+++ src/Components/BestItem.js
@@ -4,9 +4,12 @@ function BestItem() {
   return (
     <div className="BestItem">
       <h1>BEST ITEM</h1>
-      <div>
-        <h1>사진 영역</h1>
-      </div>
+      <ul className="item-area">
+        <li>개체 영역</li>
+        <li>개체 영역</li>
+        <li>개체 영역</li>
+        <li>개체 영역</li>
+      </ul>
     </div>
   );
 }
