3e9077c0c3958c95439dee3ccc92a577d119423b
Eclipse_K <godo8522@naver.com>
2024-06-12 16:19:04 +0900

    Edit width

--- src/Components/BestItem.js
+++ src/Components/BestItem.js
@@ -3,13 +3,15 @@ import "./BestItem.css";
 function BestItem() {
   return (
     <div className="BestItem">
-      <h1>BEST ITEM</h1>
-      <ul className="item-area">
-        <li>개체 영역</li>
-        <li>개체 영역</li>
-        <li>개체 영역</li>
-        <li>개체 영역</li>
-      </ul>
+      <div className="BestItem-container">
+        <h1>BEST ITEM</h1>
+        <ul className="item-area">
+          <li>개체 영역</li>
+          <li>개체 영역</li>
+          <li>개체 영역</li>
+          <li>개체 영역</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/Components/BigImg.js
+++ src/Components/BigImg.js
@@ -3,11 +3,13 @@ import "./BigImg.css";
 function BigImg() {
   return (
     <div className="BigImg">
-      <div className="BigImg-1">
-        <h1>큰 사진</h1>
-      </div>
-      <div className="BigImg-1">
-        <h1>큰 사진</h1>
+      <div className="BigImg-container">
+        <div className="BigImg-1">
+          <h1>큰 사진</h1>
+        </div>
+        <div className="BigImg-1">
+          <h1>큰 사진</h1>
+        </div>
       </div>
     </div>
   );
