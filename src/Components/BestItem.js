8cacb6e34f65ffc96305361d843568361b6f6488
Eclipse_K <godo8522@naver.com>
2024-06-16 15:22:04 +0900

    Edit page size 0616

--- src/Components/BestItem.js
+++ src/Components/BestItem.js
@@ -3,8 +3,8 @@ import "./BestItem.css";
 function BestItem() {
   return (
     <div className="BestItem">
+      <h1>BEST ITEM</h1>
       <div className="BestItem-container">
-        <h1>BEST ITEM</h1>
         <ul className="item-area">
           <li>개체 영역</li>
           <li>개체 영역</li>
