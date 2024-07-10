4a2d60db67765ff38d8708479e634f54ee02d365
Eclipse_K <godo8522@naver.com>
2024-07-10 16:29:45 +0900

    Edit LoginForm

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,54 +1,37 @@
-// src/Login.js
-import React from "react";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+// import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import "./LoginForm.css";
 
-const responseGoogle = (response) => {
-  console.log(response);
-};
+// const responseGoogle = (response) => {
+//   console.log(response);
+// };
 
 const Login = () => {
   return (
-    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100vh",
-        }}
-      >
-        <h2>Login Page</h2>
-        <form
-          style={{ display: "flex", flexDirection: "column", width: "300px" }}
-        >
-          <input
-            type="email"
-            placeholder="Email"
-            style={{ margin: "10px 0", padding: "10px", fontSize: "16px" }}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            style={{ margin: "10px 0", padding: "10px", fontSize: "16px" }}
-          />
-          <button
-            type="submit"
-            style={{ padding: "10px", fontSize: "16px", cursor: "pointer" }}
-          >
-            Login
-          </button>
-        </form>
-        <div style={{ margin: "20px 0" }}>
+    // <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+    <div className="LoginForm-container">
+      <h1 className="LoginForm-title-1">My Coffee</h1>
+      <h2 className="LoginForm-title-2">로그인</h2>
+      <form className="LoginForm-form">
+        <input className="LoginForm-input" type="email" placeholder="Email" />
+        <input
+          className="LoginForm-input"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="LoginForm-submit" type="submit">
+          Login
+        </button>
+      </form>
+      {/* <div style={{ margin: "20px 0" }}>
           <GoogleLogin
             onSuccess={responseGoogle}
             onError={() => {
               console.log("Login Failed");
             }}
           />
-        </div>
-      </div>
-    </GoogleOAuthProvider>
+        </div> */}
+    </div>
+    // </GoogleOAuthProvider>
   );
 };
 
