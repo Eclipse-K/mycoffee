6f72514c989f24fe7ff208477b0841bd4b3cbec7
Eclipse_K <godo8522@naver.com>
2024-07-14 22:43:19 +0900

    Edit LoginForm

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,26 +1,61 @@
 // import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./LoginForm.css";
+import Logo from "../images/Logo_MyCoffee.png";
 
 // const responseGoogle = (response) => {
 //   console.log(response);
 // };
 
-const Login = () => {
+function Login() {
+  const [LoginId, setLoginId] = useState("");
+  const [LoginPassword, setLoginPassword] = useState("");
+
+  const handleIdChange = (e) => {
+    setLoginId(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setLoginPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 로그인 처리 로직을 여기에 추가합니다.
+    console.log("Email:", LoginId);
+    console.log("Password:", LoginPassword);
+  };
+
   return (
     // <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
     <div className="LoginForm-container">
+      <Link to="/">
+        <img className="LoginForm-logo" src={Logo} alt="Logo" />
+      </Link>
       <h1 className="LoginForm-title-1">My Coffee</h1>
-      <h2 className="LoginForm-title-2">로그인</h2>
-      <form className="LoginForm-form">
-        <input className="LoginForm-input" type="email" placeholder="Email" />
+      <form onSubmit={handleSubmit} className="LoginForm-form">
         <input
           className="LoginForm-input"
-          type="password"
-          placeholder="Password"
+          type="LoginId"
+          placeholder="아이디"
+          value={LoginId}
+          onChange={handleIdChange}
+        />
+        <input
+          className="LoginForm-input"
+          type="LoginPassword"
+          placeholder="비밀번호"
+          value={LoginPassword}
+          onChange={handlePasswordChange}
         />
         <button className="LoginForm-submit" type="submit">
-          Login
+          로그인
         </button>
+        <div className="LoginForm-Search">
+          <Link className="LoginForm-search-id">아이디 찾기</Link>
+          <Link className="LoginForm-search-pw">비밀번호 찾기</Link>
+        </div>
       </form>
       {/* <div style={{ margin: "20px 0" }}>
           <GoogleLogin
@@ -33,6 +68,6 @@ const Login = () => {
     </div>
     // </GoogleOAuthProvider>
   );
-};
+}
 
 export default Login;
