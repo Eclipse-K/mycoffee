9ae80d3c5ce1c9b7eaf9258a97ce1ad8f3596ae3
Eclipse_K <godo8522@naver.com>
2024-07-18 15:11:51 +0900

    Add findId nav

--- src/Components/FindIdPage.js
+++ src/Components/FindIdPage.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import "./FindIdPage.css";
+import { Link } from "react-router-dom";
+import StyledLogo from "./StyledLogo";
+import Logo from "../images/Logo_MyCoffee.png";
 
 function FindIdPage() {
   const [findName, setFindName] = useState("");
@@ -50,78 +53,90 @@ function FindIdPage() {
   };
 
   return (
-    <div className="find-id-page">
-      <h2>아이디 찾기</h2>
-      <form onSubmit={handleFindSubmit}>
-        <label htmlFor="memberType">회원유형:</label>
-        <select
-          id="memberType"
-          value={memberType}
-          onChange={handleMemberTypeChange}
-        >
-          <option value="개인회원">개인회원</option>
-          <option value="기업회원">기업회원</option>
-          <option value="외국인회원">외국인회원</option>
-        </select>
+    <div className="find-id">
+      <nav className="Sign-nav">
+        <Link className="Sign-nav-logo-img" to="/">
+          <StyledLogo src={Logo} alt="Logo" />
+          <h4>아이디 찾기</h4>
+        </Link>
 
-        <div className="contact-option">
-          <input
-            type="radio"
-            id="contact-email"
-            value="email"
-            checked={contactOption === "email"}
-            onChange={handleContactOptionChange}
-          />
-          <label htmlFor="contact-email">이메일</label>
+        <Link className="Sign-nav-home" to="/">
+          <h4>MyCoffee Home</h4>
+        </Link>
+      </nav>
+      <div className="find-id-page">
+        <h2>아이디 찾기</h2>
+        <form onSubmit={handleFindSubmit}>
+          <label htmlFor="memberType">회원유형:</label>
+          <select
+            id="memberType"
+            value={memberType}
+            onChange={handleMemberTypeChange}
+          >
+            <option value="개인회원">개인회원</option>
+            <option value="기업회원">기업회원</option>
+            <option value="외국인회원">외국인회원</option>
+          </select>
 
-          <input
-            type="radio"
-            id="contact-phone"
-            value="phone"
-            checked={contactOption === "phone"}
-            onChange={handleContactOptionChange}
-          />
-          <label htmlFor="contact-phone">전화번호</label>
-        </div>
-
-        <label htmlFor="name">이름:</label>
-        <input
-          type="text"
-          id="name"
-          value={findName}
-          onChange={handleFindNameChange}
-          required
-        />
-
-        {contactOption === "email" && (
-          <>
-            <label htmlFor="email">이메일:</label>
+          <div className="contact-option">
             <input
-              type="email"
-              id="email"
-              value={findEmail}
-              onChange={handleFindEmailChange}
-              required
+              type="radio"
+              id="contact-email"
+              value="email"
+              checked={contactOption === "email"}
+              onChange={handleContactOptionChange}
             />
-          </>
-        )}
+            <label htmlFor="contact-email">이메일</label>
 
-        {contactOption === "phone" && (
-          <>
-            <label htmlFor="phone">전화번호:</label>
             <input
-              type="tel"
-              id="phone"
-              value={findPhone}
-              onChange={handleFindPhoneChange}
-              required
+              type="radio"
+              id="contact-phone"
+              value="phone"
+              checked={contactOption === "phone"}
+              onChange={handleContactOptionChange}
             />
-          </>
-        )}
+            <label htmlFor="contact-phone">전화번호</label>
+          </div>
 
-        <button type="submit">아이디 찾기</button>
-      </form>
-      {findMessage && <p>{findMessage}</p>}
+          <label htmlFor="name">이름:</label>
+          <input
+            type="text"
+            id="name"
+            value={findName}
+            onChange={handleFindNameChange}
+            required
+          />
+
+          {contactOption === "email" && (
+            <>
+              <label htmlFor="email">이메일:</label>
+              <input
+                type="email"
+                id="email"
+                value={findEmail}
+                onChange={handleFindEmailChange}
+                required
+              />
+            </>
+          )}
+
+          {contactOption === "phone" && (
+            <>
+              <label htmlFor="phone">전화번호:</label>
+              <input
+                type="tel"
+                id="phone"
+                value={findPhone}
+                onChange={handleFindPhoneChange}
+                required
+              />
+            </>
+          )}
+
+          <button type="submit">아이디 찾기</button>
+        </form>
+        {findMessage && <p>{findMessage}</p>}
+      </div>
     </div>
   );
 }
