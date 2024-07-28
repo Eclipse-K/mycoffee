2f2523b871dbe20c4d8e28565e3e749a12eeecf7
Eclipse_K <godo8522@naver.com>
2024-07-28 16:07:29 +0900

    Add FindNav.css

--- src/Components/FindIdPage.js
+++ src/Components/FindIdPage.js
@@ -3,6 +3,7 @@ import "./FindIdPage.css";
 import { Link } from "react-router-dom";
 import StyledLogo from "./StyledLogo";
 import Logo from "../images/Logo_MyCoffee.png";
+import "./FindNav.css";
 
 function FindIdPage() {
   const [findName, setFindName] = useState("");
@@ -54,88 +55,90 @@ function FindIdPage() {
 
   return (
     <div className="find-id">
-      <nav className="Sign-nav">
-        <Link className="Sign-nav-logo-img" to="/">
+      <nav className="Find-nav">
+        <Link className="Find-nav-logo-img" to="/">
           <StyledLogo src={Logo} alt="Logo" />
           <h4>아이디 찾기</h4>
         </Link>
 
-        <Link className="Sign-nav-home" to="/">
+        <Link className="Find-nav-home" to="/">
           <h4>MyCoffee Home</h4>
         </Link>
       </nav>
-      <div className="find-id-page">
-        <h2>아이디 찾기</h2>
-        <form onSubmit={handleFindSubmit}>
-          <label htmlFor="memberType">회원유형</label>
-          <select
-            id="memberType"
-            value={memberType}
-            onChange={handleMemberTypeChange}
-          >
-            <option value="개인회원">개인회원</option>
-            <option value="기업회원">기업회원</option>
-            <option value="외국인회원">외국인회원</option>
-          </select>
+      <div className="Find-id-container">
+        <div className="find-id-page">
+          <h2>아이디 찾기</h2>
+          <form onSubmit={handleFindSubmit}>
+            <label htmlFor="memberType">회원유형</label>
+            <select
+              id="memberType"
+              value={memberType}
+              onChange={handleMemberTypeChange}
+            >
+              <option value="개인회원">개인회원</option>
+              <option value="기업회원">기업회원</option>
+              <option value="외국인회원">외국인회원</option>
+            </select>
 
-          <div className="contact-option">
-            <input
-              type="radio"
-              id="contact-email"
-              value="email"
-              checked={contactOption === "email"}
-              onChange={handleContactOptionChange}
-            />
-            <label htmlFor="contact-email">이메일</label>
-
-            <input
-              type="radio"
-              id="contact-phone"
-              value="phone"
-              checked={contactOption === "phone"}
-              onChange={handleContactOptionChange}
-            />
-            <label htmlFor="contact-phone">전화번호</label>
-          </div>
-
-          <label htmlFor="name">이름</label>
-          <input
-            type="text"
-            id="name"
-            value={findName}
-            onChange={handleFindNameChange}
-            required
-          />
-
-          {contactOption === "email" && (
-            <>
-              <label htmlFor="email">이메일</label>
+            <div className="contact-option">
               <input
-                type="email"
-                id="email"
-                value={findEmail}
-                onChange={handleFindEmailChange}
-                required
+                type="radio"
+                id="contact-email"
+                value="email"
+                checked={contactOption === "email"}
+                onChange={handleContactOptionChange}
               />
-            </>
-          )}
+              <label htmlFor="contact-email">이메일</label>
 
-          {contactOption === "phone" && (
-            <>
-              <label htmlFor="phone">전화번호:</label>
               <input
-                type="tel"
-                id="phone"
-                value={findPhone}
-                onChange={handleFindPhoneChange}
-                required
+                type="radio"
+                id="contact-phone"
+                value="phone"
+                checked={contactOption === "phone"}
+                onChange={handleContactOptionChange}
               />
-            </>
-          )}
+              <label htmlFor="contact-phone">전화번호</label>
+            </div>
 
-          <button type="submit">아이디 찾기</button>
-        </form>
-        {findMessage && <p>{findMessage}</p>}
+            <label htmlFor="name">이름</label>
+            <input
+              type="text"
+              id="name"
+              value={findName}
+              onChange={handleFindNameChange}
+              required
+            />
+
+            {contactOption === "email" && (
+              <>
+                <label htmlFor="email">이메일</label>
+                <input
+                  type="email"
+                  id="email"
+                  value={findEmail}
+                  onChange={handleFindEmailChange}
+                  required
+                />
+              </>
+            )}
+
+            {contactOption === "phone" && (
+              <>
+                <label htmlFor="phone">전화번호:</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  value={findPhone}
+                  onChange={handleFindPhoneChange}
+                  required
+                />
+              </>
+            )}
+
+            <button type="submit">아이디 찾기</button>
+          </form>
+          {findMessage && <p>{findMessage}</p>}
+        </div>
       </div>
     </div>
   );
--- src/Components/FindPwPage.js
+++ src/Components/FindPwPage.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import StyledLogo from "./StyledLogo";
 import Logo from "../images/Logo_MyCoffee.png";
 import "./FindPwPage.css";
+import "./FindNav.css";
 
 function FindPwPage() {
   const [emailPw, setEmailPw] = useState("");
@@ -26,39 +27,41 @@ function FindPwPage() {
   };
 
   return (
-    <div className="FindPwPage">
-      <nav className="Sign-nav">
-        <Link className="Sign-nav-logo-img" to="/">
+    <div className="FindPw">
+      <nav className="Find-nav">
+        <Link className="Find-nav-logo-img" to="/">
           <StyledLogo src={Logo} alt="Logo" />
           <h4>비밀번호 찾기</h4>
         </Link>
 
-        <Link className="Sign-nav-home" to="/">
+        <Link className="Find-nav-home" to="/">
           <h4>MyCoffee Home</h4>
         </Link>
       </nav>
-      <div className="FindPwPage-container">
-        <h2 className="FindPwPage-title">비밀번호 찾기</h2>
-        <form className="FindPwPage-form" onSubmit={handleFindPwSubmit}>
-          <label className="FindPwPage-label">아이디</label>
-          <input
-            className="FindPwPage-input"
-            type="id"
-            value={findPwId}
-            onChange={(e) => setFindPwId(e.target.value)}
-          />
-          <label className="FindPwPage-label">이메일</label>
-          <input
-            className="FindPwPage-input"
-            type="email"
-            value={emailPw}
-            onChange={(e) => setEmailPw(e.target.value)}
-          />
-          {findPwError && <p>{findPwError}</p>}
-          <button className="FindPwPage-button" type="submit">
-            인증하기
-          </button>
-        </form>
+      <div className="FindPwPage">
+        <div className="FindPwPage-container">
+          <h2 className="FindPwPage-title">비밀번호 찾기</h2>
+          <form className="FindPwPage-form" onSubmit={handleFindPwSubmit}>
+            <label className="FindPwPage-label">아이디</label>
+            <input
+              className="FindPwPage-input"
+              type="id"
+              value={findPwId}
+              onChange={(e) => setFindPwId(e.target.value)}
+            />
+            <label className="FindPwPage-label">이메일</label>
+            <input
+              className="FindPwPage-input"
+              type="email"
+              value={emailPw}
+              onChange={(e) => setEmailPw(e.target.value)}
+            />
+            {findPwError && <p>{findPwError}</p>}
+            <button className="FindPwPage-button" type="submit">
+              인증하기
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
