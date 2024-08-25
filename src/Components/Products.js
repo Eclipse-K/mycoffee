d41b2dee2a95d8b498ce26f0f33f72cda206dfe6
Eclipse_K <godo8522@naver.com>
2024-08-25 14:47:06 +0900

    Edit Products

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import NaviBar from "./NaviBar";
 import "./Products.css";
+import CoffeeJson from "../Coffee.json";
 
 function Products() {
+  // eslint-disable-next-line
+  const [products, SetProducts] = useState(CoffeeJson.Products);
   return (
     <div className="Products">
       <NaviBar />
       <h1 className="Products-title">- Products -</h1>
       <div className="Products-container">
-        <div className="Products-area">
-          <div>img</div>
-          <p>title</p>
-          <p>price</p>
-          <p>description</p>
-        </div>
+        {products.map((pro) => (
+          <div className="Products-area" key={pro.id}>
+            <img className="Products-img" src={pro.img} alt={pro.title} />
+            <div className="Products-content">
+              <p>{pro.title}</p>
+              <p>가격 : {pro.price}</p>
+              <p>특징 : {pro.content}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
