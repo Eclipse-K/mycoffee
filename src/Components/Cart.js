a6232354d86dc11731ef0eb74eef52a5407c376b
Eclipse_K <godo8522@naver.com>
2024-09-05 09:55:50 +0900

    Edit Cart and Cart remove

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -3,6 +3,7 @@ import { CartContext } from "./CartContext";
 import Logo from "../images/Logo_MyCoffee.png";
 import "./Cart.css";
 import { Link } from "react-router-dom";
+import { MdCancelPresentation } from "react-icons/md";
 
 function Cart() {
   const { cartItems, removeFromCart } = useContext(CartContext);
@@ -40,38 +41,49 @@ function Cart() {
         <div className="Cart-Logo-title">My Coffee</div>
       </div>
       <h1 className="Cart-title">장바구니</h1>
+      <form className="Cart-form">
+        <input
+          className="Cart-all-input"
+          type="checkbox"
+          id="AllCheck"
+          name="전체선택"
+          checked={
+            isAllChecked && checkedItemIndexes.length === cartItems.length
+          }
+          onChange={handleAllCheckboxClick}
+        />
+        <label htmlFor="AllCheck">전체 선택</label>
+      </form>
       {cartItems.length === 0 ? (
         <p className="Cart-empty">장바구니가 비었습니다.</p>
       ) : (
         <div className="Cart-container">
-          <input
-            className="Cart-all-input"
-            type="checkbox"
-            checked={
-              isAllChecked && checkedItemIndexes.length === cartItems.length
-            }
-            onChange={handleAllCheckboxClick}
-          />
-          <label>전체 선택</label>
           {cartItems.map((item, index) => (
             <div className="Cart-area" key={index}>
               <input
                 className="Cart-area-input"
                 type="checkbox"
+                id={index}
+                name={index}
                 checked={checkedItemIndexes.includes(index)}
                 onChange={() => handleItemCheckboxClick(index)}
               />
               <img className="Cart-img" src={item.img} alt={item.title} />
               <div className="Cart-area-second">
                 <p className="Cart-area-title">{item.title}</p>
-                <p className="Cart-area-price">가격: {item.price}</p>
-                <button onClick={() => removeFromCart(index)}>삭제</button>
+                <p className="Cart-area-price">{item.price}</p>
+              </div>
+              <div className="Cart-remove">
+                <MdCancelPresentation onClick={() => removeFromCart(index)} />
               </div>
             </div>
           ))}
         </div>
       )}
-      <button>구매하기</button>
+      <div style={{ "padding-left": "16px" }}>
+        <button style={{ "margin-right": "8px" }}>전체구매</button>
+        <button>선택구매</button>
+      </div>
     </div>
   );
 }
