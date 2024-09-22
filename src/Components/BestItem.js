827ac349727a69cd691a05094ef1d7a7b40ac7c2
Eclipse_K <godo8522@naver.com>
2024-09-22 16:03:34 +0900

    Edit BestItem

--- src/Components/BestItem.js
+++ src/Components/BestItem.js
@@ -1,17 +1,35 @@
 import "./BestItem.css";
+import CoffeeJson from "../Coffee.json";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function BestItem() {
+  const [bestItem, setBestItem] = useState([]);
+
+  useEffect(() => {
+    const combinedItems = [
+      ...CoffeeJson.WholeBean,
+      ...CoffeeJson.DripBag,
+      ...CoffeeJson.HandDrip,
+      ...CoffeeJson.Products,
+    ];
+
+    const sortedBestItem = combinedItems
+      .sort((a, b) => b.count - a.count)
+      .slice(0, 4);
+    setBestItem(sortedBestItem);
+  }, []);
+
   return (
     <div className="BestItem">
       <div className="BestItem-container">
         <h1>BEST ITEM</h1>
         <div className="BestItem-area">
-          <ul className="item-area">
-            <li>개체 영역</li>
-            <li>개체 영역</li>
-            <li>개체 영역</li>
-            <li>개체 영역</li>
-          </ul>
+          {bestItem.map((best) => (
+            <div className="Item-area" key={best.id}>
+              <img className="Item-area-img" src={best.img} alt={best.title} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
