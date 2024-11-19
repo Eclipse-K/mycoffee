e8b327990ba19e4d7eddda03a7546534a7af935b
Eclipse_K <godo8522@naver.com>
2024-11-19 13:41:28 +0900

    Edit OrderInquiry.css

--- src/Components/MyPageFolder/OrderInquiry.js
+++ src/Components/MyPageFolder/OrderInquiry.js
@@ -40,65 +40,69 @@ function OrderInquiry({ orderActive, setOrderActive }) {
       <h1 className="order-inquiry-title">주문조회</h1>
 
       <hr />
-      <div className="order-inquiry-tabs">
-        <button
-          className={orderActive === "orderHistory" ? "active" : ""}
-          onClick={() => handleTabClick("orderHistory")}
-        >
-          주문내역조회 (0)
-        </button>
-        <button
-          className={orderActive === "cancelHistory" ? "active" : ""}
-          onClick={() => handleTabClick("cancelHistory")}
-        >
-          취소/반품/교환 내역 (0)
-        </button>
-        <button
-          className={orderActive === "pastOrderHistory" ? "active" : ""}
-          onClick={() => handleTabClick("pastOrderHistory")}
-        >
-          과거주문내역 (0)
-        </button>
-      </div>
-
-      {/* 날짜 선택 */}
-      <div className="order-inquiry-date">
-        <button onClick={() => handleQuickDateSelect(0)}>오늘</button>
-        <button onClick={() => handleQuickDateSelect(0.25)}>1주일</button>
-        <button onClick={() => handleQuickDateSelect(1)}>1개월</button>
-        <button onClick={() => handleQuickDateSelect(3)}>3개월</button>
-        <button onClick={() => handleQuickDateSelect(6)}>6개월</button>
-        <div className="order-inquiry-date-range">
-          <input
-            type="date"
-            name="startDate"
-            value={dateRange.startDate}
-            onChange={handleDateRangeChange}
-          />
-          <span>~</span>
-          <input
-            type="date"
-            name="endDate"
-            value={dateRange.endDate}
-            onChange={handleDateRangeChange}
-          />
+      <div className="order-inquiry-content">
+        <div className="order-inquiry-tabs">
+          <button
+            className={orderActive === "orderHistory" ? "active" : ""}
+            onClick={() => handleTabClick("orderHistory")}
+          >
+            주문내역조회 (0)
+          </button>
+          <button
+            className={orderActive === "cancelHistory" ? "active" : ""}
+            onClick={() => handleTabClick("cancelHistory")}
+          >
+            취소/반품/교환 내역 (0)
+          </button>
+          <button
+            className={orderActive === "pastOrderHistory" ? "active" : ""}
+            onClick={() => handleTabClick("pastOrderHistory")}
+          >
+            과거주문내역 (0)
+          </button>
         </div>
-      </div>
 
-      {/* 안내문 */}
-      <div className="order-inquiry-info">
-        <p>
-          기본적으로 최근 3개월간의 자료가 조회되며, 기간 검색시 주문처리완료 후
-          36개월 이내의 주문내역을 조회하실 수 있습니다.
-        </p>
-        <p>
-          완료 후 36개월 이상 경과한 주문은 <span>과거주문내역</span>에서 확인할
-          수 있습니다.
-        </p>
-        <p>
-          주문번호를 클릭하시면 해당 주문에 대한 상세내역을 확인하실 수
-          있습니다.
-        </p>
+        {/* 날짜 선택 */}
+        <div className="order-inquiry-calendar">
+          <div className="order-inquiry-calendar-date">
+            <button onClick={() => handleQuickDateSelect(0)}>오늘</button>
+            <button onClick={() => handleQuickDateSelect(0.25)}>1주일</button>
+            <button onClick={() => handleQuickDateSelect(1)}>1개월</button>
+            <button onClick={() => handleQuickDateSelect(3)}>3개월</button>
+            <button onClick={() => handleQuickDateSelect(6)}>6개월</button>
+          </div>
+          <div className="order-inquiry-date-range">
+            <input
+              type="date"
+              name="startDate"
+              value={dateRange.startDate}
+              onChange={handleDateRangeChange}
+            />
+            <span>~</span>
+            <input
+              type="date"
+              name="endDate"
+              value={dateRange.endDate}
+              onChange={handleDateRangeChange}
+            />
+          </div>
+        </div>
+
+        {/* 안내문 */}
+        <div className="order-inquiry-info">
+          <p>
+            기본적으로 최근 3개월간의 자료가 조회되며, 기간 검색시 주문처리완료
+            후 36개월 이내의 주문내역을 조회하실 수 있습니다.
+          </p>
+          <p>
+            완료 후 36개월 이상 경과한 주문은 <span>과거주문내역</span>에서
+            확인할 수 있습니다.
+          </p>
+          <p>
+            주문번호를 클릭하시면 해당 주문에 대한 상세내역을 확인하실 수
+            있습니다.
+          </p>
+        </div>
       </div>
 
       {/* 테이블 */}
