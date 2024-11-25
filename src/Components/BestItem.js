5060ba887f9ca9704fe4f07474caa6ce90d902ad
Eclipse_K <godo8522@naver.com>
2024-11-25 16:24:17 +0900

    Edit BestItem Click

--- src/Components/BestItem.js
+++ src/Components/BestItem.js
@@ -2,41 +2,66 @@ import "./BestItem.css";
 import CoffeeJson from "../Coffee.json";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
 
 function BestItem() {
   const [bestItem, setBestItem] = useState([]);
+  const [bestItemLoading, setBestItemLoading] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const combinedItems = [
-      ...CoffeeJson.WholeBean,
-      ...CoffeeJson.DripBag,
-      ...CoffeeJson.HandDrip,
-      ...CoffeeJson.Products,
+      ...CoffeeJson.WholeBean.map((item) => ({
+        ...item,
+        category: "WholeBean",
+      })),
+      ...CoffeeJson.DripBag.map((item) => ({ ...item, category: "DripBag" })),
+      ...CoffeeJson.HandDrip.map((item) => ({ ...item, category: "HandDrip" })),
+      ...CoffeeJson.Products.map((item) => ({ ...item, category: "Products" })),
     ];
 
     const sortedBestItem = combinedItems
       .sort((a, b) => b.count - a.count)
       .slice(0, 4);
+
     setBestItem(sortedBestItem);
   }, []);
+
+  const bestItemClick = (category, id) => {
+    window.scrollTo(0, 0);
+    setBestItemLoading(true);
+    setTimeout(() => {
+      navigate(`/${category}/${id}`);
+      setBestItemLoading(false);
+    }, 1000);
+  };
 
   return (
     <div className="BestItem">
       <div className="BestItem-container">
         <h1>BEST ITEM</h1>
-        <div className="BestItem-box">
-          <div className="BestItem-area">
-            {bestItem.map((best) => (
-              <div className="Item-area" key={best.id}>
-                <img
-                  className="Item-area-img"
-                  src={best.img}
-                  alt={best.title}
-                />
-              </div>
-            ))}
+        {bestItemLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <div className="BestItem-box">
+            <div className="BestItem-area">
+              {bestItem.map((best) => (
+                <div
+                  className="Item-area"
+                  key={best.id}
+                  onClick={() => bestItemClick(best.category, best.id)}
+                >
+                  <img
+                    className="Item-area-img"
+                    src={best.img}
+                    alt={best.title}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
