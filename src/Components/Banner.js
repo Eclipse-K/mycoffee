449954fdd1ccb44c58154f6fb2168b85d8a4a0e6
Eclipse_K <godo8522@naver.com>
2024-12-12 14:36:25 +0900

    Edit banner dot

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,51 +1,47 @@
 import "./Banner.css";
-import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 import Banner_1 from "../images/Banner1.png";
 import Banner_2 from "../images/Banner2.png";
 import Banner_3 from "../images/Banner3.png";
 import Banner_4 from "../images/Banner4.png";
-import { useCallback, useEffect, useState } from "react";
+import OpenBanner from "../images/MyCoffee_Banner_Open.svg";
+import { useEffect, useState } from "react";
 
 function Banner() {
-  const BannerImg = [Banner_1, Banner_2, Banner_3, Banner_4];
-
-  const [slideBanner, setSlideBanner] = useState(0);
-
-  const PreviousButton = () => {
-    setSlideBanner((prevIndex) =>
-      prevIndex === 0 ? BannerImg.length - 1 : prevIndex - 1
-    );
-  };
-
-  const NextButton = useCallback(() => {
-    setSlideBanner((prevIndex) =>
-      prevIndex === BannerImg.length - 1 ? 0 : prevIndex + 1
-    );
-  }, [BannerImg.length]);
+  const BannerImg = [OpenBanner, Banner_1, Banner_2, Banner_3, Banner_4];
+  const [dotBanner, setDotBanner] = useState(0);
 
   useEffect(() => {
-    const BannerTimer = setInterval(() => {
-      NextButton(); //7초마다 이동
+    const dotTimer = setInterval(() => {
+      setDotBanner((prevIndex) =>
+        prevIndex === BannerImg.length - 1 ? 0 : prevIndex + 1
+      );
     }, 7000);
+    return () => clearInterval(dotTimer);
+  }, [BannerImg.length]);
 
-    return () => clearInterval(BannerTimer);
-  }, [NextButton]); //NextButton 의존성 배열
+  const handleDotClick = (index) => {
+    setDotBanner(index);
+  };
 
   return (
     <div className="Banner">
       <div className="Banner-container">
         <div className="Banner-box">
-          <button className="Banner-prev" onClick={PreviousButton}>
-            <SlArrowLeft />
-          </button>
           <img
             className="Banner-img"
-            src={BannerImg[slideBanner]}
-            alt={`${slideBanner}`}
+            src={BannerImg[dotBanner]}
+            alt={`${dotBanner + 1}`}
           />
-          <button className="Banner-next" onClick={NextButton}>
-            <SlArrowRight />
-          </button>
+
+          <div className="dots-container">
+            {BannerImg.map((_, index) => (
+              <button
+                key={index}
+                className={`dot ${dotBanner === index ? "active" : ""}`}
+                onClick={() => handleDotClick(index)}
+              ></button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
